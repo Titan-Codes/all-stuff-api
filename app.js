74aa6dcc675d810547beca56862922024674c0ca
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-18 16:19:58 +0530

    fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.set('views', 'all-stuff-api/views/');
+app.set('views', './views/');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
